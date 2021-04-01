3412843cc1b31f303139960a5db8cd97f00aea9b
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-04-01 13:12:33 +0200

    Completed

---
+++ start.js
@@ -0,0 +1,9 @@
+//NaN stands for Not a Number.
+console.log(Number.isNaN("hello"));
+console.log(Number.isNaN(3));
+console.log(typeof NaN);
+//The type of NaN is number
+console.log((0.1 * 0.2).toFixed(2))
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
